74f7241a7a7d393575254d914890648b8a0ae849
Guilherme Barbosa <gmascb@gmail.com>
2026-01-02 18:52:01 -0300

    New folders

--- lib/flow_subscriber/version.rb
+++
@@ -1,3 +0,0 @@
-module Flows
-  VERSION = "0.0.2"
-end
--- lib/flow_subscribers.rb
+++ lib/flow_subscribers.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 require "flow_subscribers/version"
-require "flow_subscriber/simple_flow_subscriber"
-require "flow_subscriber/simple_catch_flow_subscriber"
-require "flow_subscriber/complete_flow_subscriber"
-require "flow_controller/simple_flow_controller"
-require "flow_controller/complete_flow_controller"
+require "flow_subscribers/subscriber/simple_flow_subscriber"
+require "flow_subscribers/subscriber/simple_catch_flow_subscriber"
+require "flow_subscribers/subscriber/complete_flow_subscriber"
+require "flow_subscribers/controller/simple_flow_controller"
+require "flow_subscribers/controller/complete_flow_controller"
 
 module Flows
   class Error < StandardError; end
 end
-
