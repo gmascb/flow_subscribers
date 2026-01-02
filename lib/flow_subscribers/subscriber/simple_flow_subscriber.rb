dffa1fa10d630e50fc1e8be4d670d5e299ba9070
Guilherme Barbosa <gmascb@gmail.com>
2026-01-02 18:52:47 -0300

    New folders

---
+++ lib/flow_subscribers/subscriber/simple_flow_subscriber.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Flows
+  class SimpleFlowSubscriber
+    attr_accessor :flow_context
+
+    def initialize(flow_context: {})
+      @flow_context = flow_context
+    end
+
+    # Method called by the controller - DO NOT override
+    def run(flow_context)
+      execute(flow_context)
+    end
+
+    # Method that the developer must implement
+    def execute(flow_context)
+      raise NotImplementedError, "#{self.class} must implement the execute method"
+    end
+
+    def name
+      self.class.name
+    end
+
+    def to_s
+      self.name
+    end
+  end
+end
+
